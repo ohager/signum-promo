9885c6a94057faf2b3d6dafc677de39d53e2021e
ohager <oliver.hager@dextra-sw.com>
2021-12-15 11:04:03 -0300

    feat: seo tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,17 +41,17 @@ const SaasModern = () => {
       <Fragment>
         <Head>
           <title>Signum Dev Meetup Promo</title>
-          <meta name="title" content="Signum Dev Meetup Promo"/>
-          <meta name="description" content="Checkout our upcoming Dev Meetup on Discord"/>
+          <meta name="title" content="Signum X-Mas Meetup Promo"/>
+          <meta name="description" content="Checkout our Xmas Meetup on Discord with Puzzle to win Signa"/>
           <meta property="og:type" content="website"/>
           <meta property="og:url" content="https://signum-promo.vercel.app"/>
-          <meta property="og:title" content="Signum Dev Meetup Promo"/>
-          <meta property="og:description" content="Checkout our upcoming Dev Meetup on Discord"/>
+          <meta property="og:title" content="Signum X-Mas Meetup Promo"/>
+          <meta property="og:description" content="Checkout our Xmas Meetup on Discord with Puzzle to win Signa"/>
           <meta property="og:image" content={ThumbnailUrl}/>
           <meta property="twitter:card" content="summary_large_image"/>
           <meta property="twitter:url" content="https://signum-promo.vercel.app/"/>
-          <meta property="twitter:title" content="Signum Dev Meetup Promo"/>
-          <meta property="twitter:description" content="Checkout our upcoming Dev Meetup on Discord"/>
+          <meta property="twitter:title" content="Signum X-Mas Meetup Promo"/>
+          <meta property="twitter:description" content="Checkout our Xmas Meetup on Discord with Puzzle to win Signa"/>
           <meta property="twitter:image" content={ThumbnailUrl}/>
           {/* Load google fonts */}
           <link
